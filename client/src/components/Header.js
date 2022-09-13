2da256e5528d87967b8ce9b54b4256587937016f
Won Gyun Park <wongyun@amazon.com>
2022-09-13 10:06:27 -0700

    Decode JWT token in the server instead of in the client-side

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
 import GoogleSignIn from './GoogleSignIn';
 
-
 const Header = () => {
     return (
         <div>
             <GoogleSignIn />
         </div>
-    )
-}
-
-
+    );
+};
 
 export default Header;
