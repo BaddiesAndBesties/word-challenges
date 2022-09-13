49467601a4f29cffd46d8e30a9f27c6ed92e9c76
kellichh <kellykchhe@gmail.com>
2022-09-13 16:12:57 -0400

    added layout to app sections

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import Button from './Button';
 
-const Game = () => (
-    <h1>This is where Game goes</h1>
-);
+const Game = () => {
+    const word = () => {
+        const dictionaryWord = 'potato'
+        const wordDisplay = []
+        for (let i = 0; i < dictionaryWord.length; i++) {
+            wordDisplay.push(<li className='hidden'>{dictionaryWord[i]}</li>)
+        }
+        return wordDisplay
+    }
+
+    return (
+        <main>
+            <h1>Guess the Word!</h1>
+            <div>
+                <ul>{word()}</ul>
+                <p>Incorrect Guess Counter: {} </p>
+            </div>
+            
+            <div>
+                <form action='post'>
+                    <label>Take a guess!</label>
+                    <input type='text' placeholder='Enter letter or word' required />
+                    <Button text='Submit' color='green' />
+                </form>
+            </div>
+        </main>
+    )
+}
 
 export default Game;
