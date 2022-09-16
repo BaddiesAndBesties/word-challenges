50acd7f049293d6ec86ca00e3942b6083deffa47
Will Park <park.wongyun@gmail.com>
2022-09-15 23:46:48 -0700

    update build index.html dir

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const jwtDecoder = require('./jwt');
 const { findUser, addUser, getStats, getCurrentGame, startNewGame, getTopScores } = require('../database/mongoose');
 
 const port = process.env.PORT;
-const buildDir = path.join(__dirname, '..', 'client', 'build');
+const buildDir = path.join(__dirname, '..', 'client', 'build/');
 const app = express();
 
 app.use(cors());
